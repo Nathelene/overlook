fbb4d105833add594c8a26b93879a90dff0002da
Natalie Baker <nathelene76@gmail.com>
2023-06-06 15:14:21 -0600

    add filter by date and filter by type function

--- src/bookings.js
+++ src/bookings.js
@@ -46,9 +46,54 @@ const calculateTotalCostOfUsersBookings = (customer,rooms,bookings) => {
     },0)
 }
 
+const filterRoomsByDate = (date,rooms,bookings) => {
+  if(date === "") {
+    return "INVALID DATE"
+  }
+
+  return bookings.filter(booking => {
+      return booking.date !== date 
+    
+  }).reduce((acc,booking) => {
+    rooms.forEach(room => {
+      if(room.number === booking.roomNumber) {
+   acc.push(room)
+      }
+    })
+
+    return acc      
+  },[])
+
+}
+
+const filterRoomsByType = (date,type,rooms,bookings) => {
+
+  let availableRooms = bookings.filter(booking => {
+    return booking.date !== date 
+  
+}).reduce((acc,booking) => {
+
+  rooms.forEach(room => {
+    if(room.number === booking.roomNumber && room.roomType === type) {
+ acc.push(room)
+      
+    } 
+  })
+  return acc      
+},[])
+
+if (availableRooms.length === 0) {
+return "NO ROOMS AVAILABLE"
+} 
+if (availableRooms.length > 0) {
+return availableRooms
+}
+
+}
 
 export {
     filterBookingsByUser,
     calculateTotalCostOfUsersBookings,
-
+    filterRoomsByDate,
+    filterRoomsByType
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -35,3 +35,121 @@ describe('calculateTotalCostOfUserBookings', function() {
     })
   });
 
+  describe('filterRoomsByDate', function() {
+    it('should be a function', function() {
+      expect(filterRoomsByDate).to.be.a('function');
+    });
+    it('should return rooms that are available on given date',function() {
+      const result = filterRoomsByDate("2021/11/23",roomTestData,bookingTestData)
+      expect(result).to.deep.equal([
+        {
+          number: 1,
+          roomType: 'residential suite',
+          bidet: true,
+          bedSize: 'queen',
+          numBeds: 1,
+          costPerNight: 358.4
+        },
+        {
+          number: 2,
+          roomType: 'suite',
+          bidet: false,
+          bedSize: 'full',
+          numBeds: 2,
+          costPerNight: 477.38
+        },
+        {
+          number: 3,
+          roomType: 'single room',
+          bidet: false,
+          bedSize: 'king',
+          numBeds: 1,
+          costPerNight: 491.14
+        },
+        {
+          number: 4,
+          roomType: 'single room',
+          bidet: false,
+          bedSize: 'queen',
+          numBeds: 1,
+          costPerNight: 429.44
+        },
+        {
+          number: 5,
+          roomType: 'single room',
+          bidet: true,
+          bedSize: 'queen',
+          numBeds: 2,
+          costPerNight: 340.17
+        },
+        {
+          number: 6,
+          roomType: 'junior suite',
+          bidet: true,
+          bedSize: 'queen',
+          numBeds: 1,
+          costPerNight: 397.02
+        },
+        {
+          number: 7,
+          roomType: 'single room',
+          bidet: false,
+          bedSize: 'queen',
+          numBeds: 2,
+          costPerNight: 231.46
+        },
+        {
+          number: 10,
+          roomType: 'suite',
+          bidet: false,
+          bedSize: 'twin',
+          numBeds: 1,
+          costPerNight: 497.64
+        },
+        {
+          number: 11,
+          roomType: 'single room',
+          bidet: true,
+          bedSize: 'twin',
+          numBeds: 2,
+          costPerNight: 207.24
+        }
+      ])
+    });
+    it('should return error message if date input is absent', function() {
+        const result = filterRoomsByDate("",roomTestData,bookingTestData)
+        expect(result).to.equal('INVALID DATE')
+    });
+    it('should return error message if date input is not valid', function() {
+      const result = filterRoomsByDate("20/10",roomTestData,bookingTestData)
+      expect(result).to.equal('INVALID DATE')
+  });
+  });
+  
+  describe('filterRoomsByType', function() {
+    it('should be a function', function() {
+      expect(filterRoomsByType).to.be.a('function');
+    });
+    it('should return rooms that only the current user have booked',function() {
+      const result = filterRoomsByType("2022/02/10","residential suite",roomTestData,bookingTestData)
+      expect(result).to.deep.equal([
+        {
+          number: 1,
+          roomType: 'residential suite',
+          bidet: true,
+          bedSize: 'queen',
+          numBeds: 1,
+          costPerNight: 358.4
+        }
+      ])
+    });
+    it('should return error message if room type is unavailable', function() {
+        const result = filterRoomsByType("2022/02/10","king room",roomTestData,bookingTestData)
+        expect(result).to.equal('NO ROOMS AVAILABLE')
+    });
+    it('should return error message if room type is not selected', function() {
+      const result = filterRoomsByType("2022/02/10","",roomTestData,bookingTestData)
+      expect(result).to.equal('NO ROOMS AVAILABLE')
+  });
+  });
+ 
